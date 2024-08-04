34da6cf2e4c88793b1286d9d8207f3887d48f2d6
Pranav <90679961+pranav-rajput@users.noreply.github.com>
2024-08-05 04:35:45 +0530

    Time: 0 ms (100%), Space: 41.8 MB (76.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,13 +3,13 @@ class Solution {
         int search(int[] nums, int target) {
         int pivot = findPivot(nums);
 
-        // if you did not find a pivot, it means the array is not rotated
+       
         if (pivot == -1) {
-            // just do normal binary search
+            
             return binarySearch(nums, target, 0 , nums.length - 1);
         }
 
-        // if pivot is found, you have found 2 asc sorted arrays
+        
         if (nums[pivot] == target) {
             return pivot;
         }
@@ -23,8 +23,7 @@ class Solution {
 
     int binarySearch(int[] arr, int target, int start, int end) {
         while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+
             int mid = start + (end - start) / 2;
 
             if (target < arr[mid]) {
@@ -32,20 +31,19 @@ class Solution {
             } else if (target > arr[mid]) {
                 start = mid + 1;
             } else {
-                // ans found
+               
                 return mid;
             }
         }
         return -1;
     }
 
-    // this will not work in duplicate values
     int findPivot(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            // 4 cases over here
+            
             if (mid < end && arr[mid] > arr[mid + 1]) {
                 return mid;
             }
@@ -66,7 +64,7 @@ class Solution {
         int end = arr.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            // 4 cases over here
+          
             if (mid < end && arr[mid] > arr[mid + 1]) {
                 return mid;
             }
@@ -74,23 +72,20 @@ class Solution {
                 return mid-1;
             }
 
-            // if elements at middle, start, end are equal then just skip the duplicates
             if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
-                // skip the duplicates
-                // NOTE: what if these elements at start and end were the pivot??
-                // check if start is pivot
+
                 if (start < end && arr[start] > arr[start + 1]) {
                     return start;
                 }
                 start++;
 
-                // check whether end is pivot
+               
                 if (end > start && arr[end] < arr[end - 1]) {
                     return end - 1;
                 }
                 end--;
             }
-            // left side is sorted, so pivot should be in right
+          
             else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
                 start = mid + 1;
             } else {
